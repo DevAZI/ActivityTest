ef8d464ca8c49dc1dba6e857ce7e986aa78d861e
DevAZI <80841284+DevAZI@users.noreply.github.com>
2021-11-09 01:48:26 +0900

    intent 변경 onstart..등 생명주기 확인

--- app/src/main/java/com/example/activitytest/MainActivity.java
+++ app/src/main/java/com/example/activitytest/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.activitytest;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,7 +31,7 @@ public class MainActivity extends AppCompatActivity {//AppCompatActivity
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               int a = Integer.parseInt(editText1.getText().toString());
+                int a = Integer.parseInt(editText1.getText().toString());
                 int b = Integer.parseInt(editText2.getText().toString());
                 Log.i(TAG, "onClick");
                 Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
@@ -35,22 +39,72 @@ public class MainActivity extends AppCompatActivity {//AppCompatActivity
                 intent.putExtra("data1", a);
                 intent.putExtra("data2", b);
                 //startActivity(intent);
-                startActivityForResult(intent, 0);
+                //startActivityForResult(intent, 0);
+                startActivityResult.launch(intent);
             }
         });
 
     }
+    ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
 
+                    if (result.getResultCode() == RESULT_OK) {
+                        Log.i(TAG, "ok");
+                    }
+
+                }
+            }
+    );
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == 0) {
+//            if (requestCode == RESULT_OK) {
+//                int result = data.getIntExtra("sum", 0);
+//
+//            }
+//        }
+//    }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 0) {
-            if (requestCode == RESULT_OK) {
-                int result = data.getIntExtra("sum", 0);
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
 
-            }
-        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
     }
 
 }
--- app/src/main/java/com/example/activitytest/SecondActivity.java
+++ app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -37,20 +37,62 @@ public class SecondActivity extends AppCompatActivity {
             }
         });
 
+        //값 가져오기
         Button btnSum = findViewById(R.id.sum);
         btnSum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int a = getIntent().getIntExtra("data1", 0);
                 int b = getIntent().getIntExtra("data2", 0);
+
                 Log.i(TAG, "a=" + a);
                 Log.i(TAG, "b=" + b);
                 int sum = a + b;
+                Log.i(TAG, "sum" + sum);
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 intent.putExtra("sum", sum);
+                intent.getStringExtra("data");
                 setResult(RESULT_OK,intent);
                 finish();
             }
         });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
     }
 }
